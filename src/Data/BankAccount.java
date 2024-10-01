e11a2065fe10a005e15a76002da78bfd4877ecbb
Alejo Madrid Giral <alejomadridg@hotmail.com>
2024-10-01 16:36:12 -0500

    feat choose operation add or retrive money from account

--- src/Data/BankAccount.java
+++ src/Data/BankAccount.java
@@ -5,11 +5,27 @@ public class BankAccount {
         public String personName;
         public double accountAmount = 0;
 
-        public void addMoneytoAccount(double addMoney) {
-                accountAmount += addMoney;
+        public void addMoneytoAccount(double amount) {
+                accountAmount += amount;
         }
 
-        public void retrieveMoneytoAccount(double retrieveMoney) {
-                accountAmount -= retrieveMoney;
+        public void retrieveMoneytoAccount(double amount) {
+                accountAmount -= amount;
+        }
+
+        public void chooseOperation(int choice, double amount) {
+
+                // Perform the operation based on the user's choice.
+                switch (choice) {
+                        case 1:
+                                addMoneytoAccount(amount);
+                                break;
+                        case 2:
+                                retrieveMoneytoAccount(amount);
+                                break;
+                        default:
+                                System.out.println("Opción no válida.");
+                                break;
+                }
         }
 }
